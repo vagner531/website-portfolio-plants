d2136416d7a6d9413234417b91a7f0f926679634
vagner santos <vagner131417@gmail.com>
2022-08-23 00:23:52 -0300

    Criado Produtos

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@ if (navClose) {
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav_link')
 
 function linkAction() {
     const navMenu = document.getElementById('nav-menu')
@@ -28,3 +28,13 @@ function linkAction() {
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+        // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 80) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
